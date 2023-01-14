0fd88139d5ba2d6d9e564f3128d62a4bdbb55462
Truc Lam <truc.lamg@gmail.com>
2023-01-15 00:09:24 +0100

    Add and link search bar in map activity

--- app/src/main/java/org/wit/placemark/activities/MapActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/MapActivity.kt
@@ -2,8 +2,11 @@ package org.wit.placemark.activities
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,6 +17,8 @@ import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.placemark.R
 import org.wit.placemark.databinding.ActivityMapBinding
 import org.wit.placemark.models.Location
+import java.io.IOException
+
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener {
 
@@ -29,6 +34,56 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerD
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        val searchView = findViewById<SearchView>(R.id.search_view)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // Perform the search here
+                val loc = searchView.getQuery().toString()
+                // below line is to create a list of address
+                // where we will store the list of all address.
+                // below line is to create a list of address
+                // where we will store the list of all address.
+                var addressList: List<Address>? = null
+
+                // checking if the entered location is null or not.
+
+                // checking if the entered location is null or not.
+                if (loc != "") {
+                    // on below line we are creating and initializing a geo coder.
+                    val geocoder = Geocoder(this@MapActivity)
+                    try {
+                        // on below line we are getting location from the
+                        // location name and adding that location to address list.
+                        addressList = geocoder.getFromLocationName(loc, 1)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                    // on below line we are getting the location
+                    // from our list a first position.
+                    val address: Address = addressList!![0]
+
+                    // on below line we are creating a variable for our location
+                    // where we will add our locations latitude and longitude.
+                    val latLng = LatLng(address.getLatitude(), address.getLongitude())
+
+                    // on below line we are adding marker to that position.
+                    map.addMarker(MarkerOptions().position(latLng).title(loc))
+
+                    // below line is to animate camera to that position.
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+
+                    location.lat = address.getLatitude()
+                    location.lng = address.getLongitude()
+                    location.zoom = map.cameraPosition.zoom
+
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
